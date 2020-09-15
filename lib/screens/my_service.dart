84fabafb45a8c66b6b81acdd2e774b4644c55f35
ajhol <abdloh@gmail.com>
2020-09-15 21:26:11 +0700

    78

--- lib/screens/my_service.dart
+++ lib/screens/my_service.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:ex1/screens/home.dart';
 
 class MyService extends StatefulWidget {
   @override
@@ -6,11 +8,66 @@ class MyService extends StatefulWidget {
 }
 
 class _MyServiceState extends State<MyService> {
+  //Explicit
+
+  // Method
+
+  Widget signOutButton() {
+    return IconButton(
+      icon: Icon(Icons.exit_to_app),
+      tooltip: 'Sign Out',
+      onPressed: () {
+        myAlert();
+      },
+    );
+  }
+
+  void myAlert() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text('Are You Sure ?'),
+            content: Text('Do You Want Sign Out?'),
+            actions: [cancelButton(), okButton()],
+          );
+        });
+  }
+
+  Widget okButton() {
+    return FlatButton(
+      onPressed: () {
+        Navigator.of(context).pop();
+        processSignOut();
+      },
+      child: Text('OK'),
+    );
+  }
+
+  Future<void> processSignOut() async {
+    FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+    await firebaseAuth.signOut().then((response) {
+      MaterialPageRoute materialPageRoute =
+          MaterialPageRoute(builder: (BuildContext context) => Home());
+      Navigator.of(context).pushAndRemoveUntil(
+          materialPageRoute, (Route<dynamic> route) => false);
+    });
+  }
+
+  Widget cancelButton() {
+    return FlatButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        child: Text('Cancel'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('My Service'),
+        actions: [signOutButton()],
       ),
       body: Text('Body'),
     );
