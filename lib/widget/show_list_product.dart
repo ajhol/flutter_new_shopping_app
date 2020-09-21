adf4ad70c5d9c1f1863808b407f493cb1c7805b2
ajhol <abdloh@gmail.com>
2020-09-21 11:30:26 +0700

    ere

--- lib/widget/show_list_product.dart
+++ lib/widget/show_list_product.dart
@@ -35,13 +35,62 @@ class _ShowListProductState extends State<ShowListProduct> {
     });
   }
 
+  Widget showImage(int index) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.5,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          image: DecorationImage(
+            image: NetworkImage(productModels[index].pathImage),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget showName(int index) {
+    return Text(productModels[index].name);
+  }
+
+  Widget showDetail(int index) {
+    String string = productModels[index].detail;
+    if (string.length > 100) {
+      string = string.substring(0, 99);
+      string = '$string...';
+    }
+    return Text(string);
+  }
+
+  Widget showText(int index) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      height: MediaQuery.of(context).size.width * 0.5,
+      child: Column(
+        children: [showName(index), showDetail(index)],
+      ),
+    );
+  }
+
+  Widget showListView(int index) {
+    return Row(
+      children: [
+        showImage(index),
+        showText(index),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         child: ListView.builder(
       itemCount: productModels.length,
       itemBuilder: (BuildContext buildContext, int index) {
-        return Text(productModels[index].name);
+        return showListView(index);
       },
     ));
   }
