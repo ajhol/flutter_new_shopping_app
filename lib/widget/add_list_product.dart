cc64bba8a7d26fd4b9304461bfd253618a011f14
ajhol <abdloh@gmail.com>
2020-09-21 12:13:34 +0700

    6565

--- lib/widget/add_list_product.dart
+++ lib/widget/add_list_product.dart
@@ -8,10 +8,36 @@ class AddListProduct extends StatefulWidget {
 }
 
 class _AddListProductState extends State<AddListProduct> {
+  //Field
+
+  //Method
+
+  Widget showImage() {
+    return Container(
+      padding: EdgeInsets.all(20),
+      // color: Colors.grey,
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.3,
+      child: Image.asset('images/pic.png'),
+    );
+  }
+
+  Widget showContent() {
+    return Column(
+      children: [
+        showImage(),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('this is addlistProduct'),
+      child: Stack(
+        children: [
+          showContent(),
+        ],
+      ),
     );
   }
 }
