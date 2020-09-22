a8c951da0938800d171aeb28fe5febc97182a815
ajhol <abdloh@gmail.com>
2020-09-22 11:37:46 +0700

    34

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Loh Shop',
       theme: ThemeData(primarySwatch: Colors.deepOrange),
       home: Home(),
     );
